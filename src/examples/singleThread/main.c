7c9eb62c7a5d021e4b84201af8b4c0592806a7e0
Dean Sutton <d.n.sutton@gmail.com>
2016-01-21 23:53:24 +0000

    Adding a simple example.

--- src/examples/singleThread/main.c
+++ src/examples/singleThread/main.c
@@ -14,31 +14,15 @@
  * You should have received a copy of the GNU General Public License
  * along with Log4C.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef LOG4C_H
-#define LOG4C_H
+#include <log4c.h>
+#include <stdlib.h>
 
-#include <stdint.h>
-
-typedef struct
+int main(void)
 {
-    void (*error)(char *fmt, ...);
-    void (*warning)(char *fmt, ...);
-    void (*info)(char *fmt, ...);
-    void (*debug)(char *fmt, ...);
-    struct
-    {
-        struct
-        {
-            uint32_t size;
-            uint32_t highWaterMark;
-        } buffer;
-    } stats;
-} Log4C_t;
+    LOG.error("Hello world! I am error number %u.\n", 22);
+    LOG.warning("Okay I am a hot pie. You have been warned %d times.\n", -23);
+    LOG.info("I am %s. Talk to the %s....\n", "TERMINAL", "KEYBOARD");
+    LOG.debug("I like to fill up %s %u times.\n", "log files", 100000);
 
-/**
- * Global instance of Log4C. Assuming this module has been ported correctly
- * this global object can be used at any time.
- */
-extern Log4C_t LOG;
-
-#endif /* LOG4C_H */
+    return EXIT_SUCCESS;
+}
--- src/log4c/log4c.c
+++ src/log4c/log4c.c
@@ -1,11 +1,28 @@
-
+/**
+ * This file is part of Log4C.
+ * 
+ * Log4C is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Log4C is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Log4C.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "log4c.h"
+
+#include <stdio.h>
+#include <stdarg.h>
 
 void Log4C_Error(char *fmt, ...);
 void Log4C_Warning(char *fmt, ...);
 void Log4C_Info(char *fmt, ...);
 void Log4C_Debug(char *fmt, ...);
-
 
 Log4C_t LOG = {
     .error = Log4C_Error,
@@ -22,20 +39,41 @@ Log4C_t LOG = {
 
 void Log4C_Error(char *fmt, ...)
 {
-    
+    va_list args;
+
+    printf("ERROR: ");
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
 }
 
 void Log4C_Warning(char *fmt, ...)
 {
-    
+    va_list args;
+
+    printf("WARNING: ");
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
 }
 
 void Log4C_Info(char *fmt, ...)
 {
-    
+    va_list args;
+
+    printf("INFO: ");
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+
 }
 
 void Log4C_Debug(char *fmt, ...)
 {
-    
+    va_list args;
+
+    printf("DEBUG: ");
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
 }
